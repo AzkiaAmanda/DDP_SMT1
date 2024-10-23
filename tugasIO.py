90a742bbbdc530abb19c188734d78c65d8a15711
AzkiaAmanda <azkiaamanda03@gmail.com>
2024-10-23 09:11:12 +0700

    Add files via upload

---
+++ tugasIO.py
@@ -0,0 +1,41 @@
+#Input data
+print("Data Pegawai PT. XYZ")
+print("*Silahkan Input Menggunakan Awalan Huruf Kapital")
+nama = input("Masukan Nama: ")
+agama = input("Masukan Agama: ")
+divisi = input("Masukan Divisi: ")
+jabatan = input("Masukan Jabatan: ")
+
+#Menentukan gaji pokok sesuai jabatan
+if jabatan == "Staff":
+   gapok = 4000000
+elif jabatan == "Kabag":
+   gapok = 7000000
+elif jabatan == "Manajer":
+   gapok = 10000000
+else:
+   gapok = 0
+
+#Hitung tunjangan jabatan
+tunjab = 0.2 * gapok
+
+#Hitung gaji kotor
+gator = gapok + tunjab
+
+#Hitung pengeluaran zakat
+zapro = (0.025 * gator) if (agama.lower() == "islam" and gator >= 7000000 ) else 0
+
+#hitung gaji bersih
+gasih = (gapok + tunjab) - zapro
+
+#Cetak
+print("\nHasil Sistem Penggajian PT. XYZ")
+print(f"Nama               : {nama}")
+print(f"Agama              : {agama}")
+print(f"Divisi             : {divisi}")
+print(f"Jabatan            : {jabatan}")
+print(f"Gaji Pokok         : Rp {gapok:,.2f}")
+print(f"Tunjangan Jabatan  : Rp {tunjab:,.2f}")
+print(f"Gaji Kotor         : Rp {gator:,.2f}")
+print(f"Zakat Profesi      : Rp {zapro:,.2f}")
+print(f"Gaji Bersih        : {gasih:,.2f}")

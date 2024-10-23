f3cad34b393fef6fd0d85ba1c544fa85b4e4a75c
AzkiaAmanda <azkiaamanda03@gmail.com>
2024-10-23 09:12:52 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,43 @@
+#buatlah rumus menghitung luas Persegi
+sisi = 6
+luasPersegi = sisi * sisi
+print("Luas Persegi", luasPersegi)
+
+#buatlah rumus menghitung luas Persegi Panjang
+panjang = 7
+lebar = 5
+luasPersegiPanjang = panjang * lebar
+print("Luas Persegi Panjang", luasPersegiPanjang)
+
+#buatlah rumus menghitung luas Segitiga
+alas = 6
+tinggi = 9
+luasSegitiga = alas * tinggi / 2
+print("Luas Segitiga", luasSegitiga)
+
+#buatlah rumus menghitung luas Lingkaran
+jari2 = 7
+diameter = 14
+PHI = 3.14
+luasLingkaran1 = PHI * jari2 * jari2
+luasLingkaran2 = PHI * diameter * diameter / 4
+print("Luas Lingkaran 1", luasLingkaran1,
+    "\nLuas Lingkaran 2", luasLingkaran2)
+
+#buat perbadingan lebih besar dan lebih kecil dari luas persegi dan luas persegi panjang
+luasPersegi = 36
+luasPersegiPanjang = 35
+print("Perbandingan Luas Persegi dan Persegi Panjang:")
+print(luasPersegi < luasPersegiPanjang)
+print(luasPersegi > luasPersegiPanjang)
+print(luasPersegiPanjang < luasPersegi)
+print(luasPersegiPanjang > luasPersegi)
+
+#buat perbadingan lebih besar dan lebih kecil dari Segitiga dan Lingkaran
+luasSegitiga = 27
+luasLingkaran =153.86
+print("Perbandingan Luas Persegi dan Persegi Panjang:")
+print(luasSegitiga < luasLingkaran)
+print(luasSegitiga > luasLingkaran)
+print(luasSegitiga < luasLingkaran)
+print(luasSegitiga > luasSegitiga)
